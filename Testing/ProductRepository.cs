d0f7ba2d6023281b67770126fea5fa6ecf84c9f9
Adam Johnson <johnson.adamdouglas@gmail.com>
2022-11-23 12:14:05 -0500

    completed exercise 1

---
+++ Testing/ProductRepository.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using Testing.Models;
+using System.Data;
+using Dapper;
+
+namespace Testing
+{
+    public class ProductRepository : IProductRepository
+    {
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return _conn.Query<Product>("SELECT * FROM PRODUCTS;");
+        }
+
+        private readonly IDbConnection _conn;
+        public ProductRepository(IDbConnection conn)
+        {
+            _conn = conn;
+        }
+    }
+}
